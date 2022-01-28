c7503eddd373bf569142ffbea093e55db0ca2dd1
Connor Slade <connor@connorcode.com>
2022-01-28 13:19:49 -0500

    🧁 Progress

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
-use std::collections::HashMap;
 use std::fs;
 
-use afire::{Logger, Method, Middleware, Query, Response, ServeStatic, Server};
+use afire::{Content, Logger, Method, Middleware, Response, ServeStatic, Server};
+use chrono::prelude::*;
 use mut_static::MutStatic;
 use uuid::Uuid;
 #[macro_use]
 extern crate lazy_static;
 
+const DATA_LIMIT: usize = 256_000;
+
+#[derive(Debug)]
+pub struct Bin {
+    uuid: Uuid,
+    data: String,
+    name: String,
+    time: DateTime<Utc>,
+}
+
 lazy_static! {
-    pub static ref DATA: MutStatic<HashMap<Uuid, String>> = MutStatic::from(HashMap::new());
+    pub static ref DATA: MutStatic<Vec<Bin>> = MutStatic::from(Vec::new());
 }
 
 fn main() {
@@ -23,9 +33,25 @@ fn main() {
             None => return Response::new().text("Invalid Text"),
         };
 
-        let uuid = Uuid::new_v4();
+        if body_str.len() > DATA_LIMIT {
+            return Response::new().text("Data too big!");
+        }
 
-        DATA.write().unwrap().insert(uuid, body_str);
+        let name = req
+            .header("Name")
+            .unwrap_or_else(|| "Untitled Bin".to_owned());
+
+        let uuid = Uuid::new_v4();
+        let bin = Bin {
+            uuid,
+            name,
+            data: body_str,
+            time: Utc::now(),
+        };
+
+        dbg!(&bin);
+
+        DATA.write().unwrap().push(bin);
         dbg!(&*DATA.read().unwrap());
 
         Response::new().text(uuid)
@@ -35,18 +61,58 @@ fn main() {
         let id = req.path_param("id").unwrap();
         let data = DATA.read().unwrap();
 
-        let data = match data.get(&Uuid::parse_str(&id).unwrap()) {
+        let uuid = &Uuid::parse_str(&id).unwrap();
+        let data = match data.iter().find(|x| x.uuid == *uuid) {
             Some(i) => i,
             None => return Response::new().status(404),
         };
 
         let template = fs::read_to_string("web/template/box.html")
             .unwrap()
-            .replace("{{DATA}}", data)
+            .replace("{{DATA}}", &safe_html(&data.data))
             .replace("{{ID}}", &id);
 
         Response::new().text(template)
     });
 
+    server.route(Method::GET, "/raw/{id}", |req| {
+        let id = req.path_param("id").unwrap();
+        let data = DATA.read().unwrap();
+
+        let uuid = &Uuid::parse_str(&id).unwrap();
+        let data = match data.iter().find(|x| x.uuid == *uuid) {
+            Some(i) => i,
+            None => return Response::new().status(404),
+        };
+
+        Response::new().text(&data.data).content(Content::TXT)
+    });
+
+    server.route(Method::GET, "/recent", |_| {
+        let data = DATA.read().unwrap();
+        let mut out = String::new();
+
+        for (i, item) in (&*data).iter().enumerate() {
+            if i > 15 {
+                break;
+            }
+
+            out.push_str(&item.uuid.to_string());
+            out.push_str("<br>")
+        }
+
+        let template = fs::read_to_string("web/template/recent.html")
+            .unwrap()
+            .replace("{{DATA}}", &out);
+
+        Response::new().text(template).content(Content::HTML)
+    });
+
     server.start().unwrap();
+}
+
+fn safe_html(html: &str) -> String {
+    html.replace('&', "&amp;")
+        .replace('<', "&lt;")
+        .replace('>', "&gt;")
 }
--- web/static/new/index.js
+++ web/static/new/index.js
@@ -1,12 +1,26 @@
 let data = {
   submit: () => {
-    let data = document.querySelector("#form").elements.text.value;
+    document.querySelector("#submit").classList.add("is-loading");
+    let data = document.querySelector("#form").elements;
+    localStorage.removeItem("text");
 
     fetch("/new", {
       method: "post",
-      body: data,
+      body: data.text.value,
+      headers: {Name: data.name.value},
     })
       .then((d) => d.text())
-      .then((d) => (window.location.pathname = `/b/${d}`));
+      .then((d) => (window.location.pathname = `/b/${d}`))
+      .catch(err);
   },
 };
+
+window.addEventListener("load", () => {
+  const textBox = document.querySelector(".text-input");
+
+  textBox.value = localStorage.getItem("text");
+
+  textBox.addEventListener("input", (e) => {
+    localStorage.setItem("text", e.target.value);
+  });
+});
